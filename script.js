233ab1bc5ddae5a72c7b2aa67e9cf5ddbbf91318
Caleb Stanfield <finn0010@gmail.comm>
2026-02-26 04:43:24 -0600

    Added theme toggle functions, unfinished.

--- script.js
+++ script.js
@@ -4,12 +4,19 @@ const bodyElement = document.body;
 const toggleButton = document.getElementById('toggle-theme');
 const extensionSection = document.getElementById('extensions');
 const mainDiv = extensionSection.querySelectorAll('div');
+const divCard = document.querySelectorAll('.card');
 const mainElement = document.main;
 
 
 function toggleTheme() {
-  bodyElement.classList.toggle('dark-theme');
-  mainDiv.forEach(div => div.classList.toggle('dark-card'));
-}
+    bodyElement.classList.toggle('dark-theme');
+    mainDiv.forEach(div => {
+        div.classList.toggle('dark-card');
+    });
+
+    if (div.classList.contains('card')) {
+        div.classList.toggle('dark-card');
+    }
+};  
 
 toggleButton.addEventListener('click', toggleTheme);
